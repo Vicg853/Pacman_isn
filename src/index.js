745f588be692037ded6bd7235d103b53abb21b65
Victor Gomez <vicg853@gmail.com>
2020-11-08 15:05:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ const api = express();
 
 api.use(express.static("static"));
 
-api.listen(3000, () => console.log("Server is listening in http://localhost:3000"));
+api.listen(8080, () => console.log("Server is listening in http://localhost:3000"));
